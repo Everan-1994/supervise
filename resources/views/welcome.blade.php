93f1a3cbe3bc71b2e10acd64632a23af1307b435
vagrant <vagrant@homestead>
2017-11-21 06:57:13 +0000

    t

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Supervise
                 </div>
 
                 <div class="links">
