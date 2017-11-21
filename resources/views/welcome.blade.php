eca48a80dc2cb9600316525171138b4db320fa77
vagrant <vagrant@homestead>
2017-11-21 10:44:01 +0000

    t

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Supervise
+                    Supervise Let'go!!!
                 </div>
 
                 <div class="links">
